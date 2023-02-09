de4babe19a28c0c64a9e5f029a44b150c3ed0059
啊这！ <oncwnuISEQ3CquMPjbBKsppssthI@git.weixin.qq.com>
2023-02-09 19:31:49 +0800

    初始化项目

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -4,7 +4,7 @@ import { userType } from "./types";
 import { routerArrays } from "@/layout/types";
 import { router, resetRouter } from "@/router";
 import { storageSession } from "@pureadmin/utils";
-import { getLogin, refreshTokenApi } from "@/api/user";
+import { refreshTokenApi, login } from "@/api/user";
 import { UserResult, RefreshTokenResult } from "@/api/user";
 import { useMultiTagsStoreHook } from "@/store/modules/multiTags";
 import { type DataInfo, setToken, removeToken, sessionKey } from "@/utils/auth";
@@ -30,7 +30,7 @@ export const useUserStore = defineStore({
     /** 登入 */
     async loginByUsername(data) {
       return new Promise<UserResult>((resolve, reject) => {
-        getLogin(data)
+        login(data)
           .then(data => {
             if (data) {
               setToken(data.data);
