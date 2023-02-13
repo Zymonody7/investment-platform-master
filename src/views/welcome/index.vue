96301d6971cac1be86b60ebc00b7d538fe7df979
lonelysnowman <2438490168@qq.com>
2023-02-13 21:03:41 +0800

    feat: 首页嵌套为汾飞招商可视化大屏

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -2,46 +2,19 @@
 defineOptions({
   name: "Welcome"
 });
+const HomeUrl = import.meta.env.VITE_HOME_URL;
+console.log(import.meta);
 </script>
 
 <template>
   <div class="homeBox">
-    <div class="illustration" />
-    <div class="icon" />
-    <h1 class="font">招商平台管理端</h1>
+    <iframe :src="HomeUrl" style="height: 100%; width: 100%" />
   </div>
 </template>
 
 <style scoped>
 .homeBox {
   height: 80vh;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  background: url("@/assets/login/bg.png");
-  background-size: cover;
-}
-
-.icon {
-  height: 30vh;
-  width: 30vh;
-  background: url("@/assets/login/avatar.svg");
-  background-size: cover;
-}
-
-.illustration {
-  background: url("@/assets/login/illustration.svg");
-  background-size: cover;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-}
-
-.font {
-  font-size: 2rem;
+  box-sizing: border-box;
 }
 </style>
