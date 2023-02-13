1ec39f4455e9c8d76c13d70a026badcb1241d781
lonelysnowman <2438490168@qq.com>
2023-02-13 20:17:33 +0800

    feat: 修改页面内容，进行最后打包

--- src/router/modules/park.ts
+++ src/router/modules/park.ts
@@ -16,15 +16,7 @@ export default {
       name: "Park",
       component: () => import("@/views/park/index.vue"),
       meta: {
-        title: "园区列表"
-      }
-    },
-    {
-      path: "/parkpreview",
-      name: "Park1",
-      component: () => import("@/views/park/image.vue"),
-      meta: {
-        title: "园区预览"
+        title: "园区管理"
       }
     }
   ]
--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -5,5 +5,43 @@ defineOptions({
 </script>
 
 <template>
-  <h1>招商平台管理端</h1>
+  <div class="homeBox">
+    <div class="illustration" />
+    <div class="icon" />
+    <h1 class="font">招商平台管理端</h1>
+  </div>
 </template>
+
+<style scoped>
+.homeBox {
+  height: 80vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url("@/assets/login/bg.png");
+  background-size: cover;
+}
+
+.icon {
+  height: 30vh;
+  width: 30vh;
+  background: url("@/assets/login/avatar.svg");
+  background-size: cover;
+}
+
+.illustration {
+  background: url("@/assets/login/illustration.svg");
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.font {
+  font-size: 2rem;
+}
+</style>
